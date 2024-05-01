25b966c1b9baa7df5ff952a66a01143a147fe12f
Marius <tweety.04.01@gmail.com>
2024-04-30 20:57:42 -0400

    timing fixed

--- Cute Video Editor/App.xaml.cs
+++ Cute Video Editor/App.xaml.cs
@@ -83,43 +83,43 @@ public partial class App : Application
             .ConfigureLogging((context, config) =>
                 config.AddConfiguration(context.Configuration.GetSection("Logging")))
             .Build();
-
+        
         UnhandledException += App_UnhandledException;
 
         //FFmpegInteropLogging.SetLogLevel(FFmpegInteropX.LogLevel.Trace);
         FFmpegInteropLogging.SetLogProvider(GetService<FFmpegLogProvider>());
 
-        var vm = GetService<MainViewModel>();
-        vm.LoadProjectFile(@"E:\gitrepos\Cute Video Editor\Samples\sana anime girl.cve");
-
-        using var transcoder = new FFmpegTranscode();
-        var outputParameters = new FFmpegTranscodeOutput
-        {
-            FileName = @"d:\temp\test-cve.webm",
-            Type = OutputType.Vp9,
-            CRF = 15,
-            FrameRate = 30,
-            PixelSize = new(vm.LargestOutputPixelSize.Width, vm.LargestOutputPixelSize.Height),
-            Preset = OutputPresetType.Medium,
-        };
-        transcoder.Run(new()
-        {
-            FileName = vm.MediaFileName!,
-            CropFrames = vm.CropFrames.Select(w => new FFmpegTranscodeInputCropFrameEntry(
-                w.FrameNumber, new(w.Rect.CenterX, w.Rect.CenterY, w.Rect.Width, w.Rect.Height))).ToList(),
-            TrimmingMarkers = vm.TrimmingMarkers.Select(w => new FFmpegTranscodeInputTrimmingMarkerEntry(
-                w.FrameNumber, w.TrimAfter)).ToList()
-        }, outputParameters);
+        //var vm = GetService<MainViewModel>();
+        //vm.LoadProjectFile(@"E:\gitrepos\CuteVideoEditor\Samples\sana anime girl.cve");
+        
+        //using var transcoder = new FFmpegTranscode();
+        //var outputParameters = new FFmpegTranscodeOutput
+        //{
+        //    FileName = @"d:\temp\test-cve.webm",
+        //    Type = OutputType.Vp9,
+        //    CRF = 15,
+        //    FrameRate = 30,
+        //    PixelSize = new(vm.LargestOutputPixelSize.Width, vm.LargestOutputPixelSize.Height),
+        //    Preset = OutputPresetType.Medium,
+        //};
+        //transcoder.Run(new()
+        //{
+        //    FileName = vm.MediaFileName!,
+        //    CropFrames = vm.CropFrames.Select(w => new FFmpegTranscodeInputCropFrameEntry(
+        //        w.FrameNumber, new(w.Rect.CenterX, w.Rect.CenterY, w.Rect.Width, w.Rect.Height))).ToList(),
+        //    TrimmingMarkers = vm.TrimmingMarkers.Select(w => new FFmpegTranscodeInputTrimmingMarkerEntry(
+        //        w.FrameNumber, w.TrimAfter)).ToList()
+        //}, outputParameters);
     }
-
+    
     private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
     {
     }
-
+    
     protected async override void OnLaunched(LaunchActivatedEventArgs args)
     {
         base.OnLaunched(args);
-
+        
         await GetService<IActivationService>().ActivateAsync(args);
     }
 }
